f028a0d023fd85ef7eae47340c0aab2f00991cf4
pranshhhupg <guptaa.pranshu@gmail.com>
2026-04-02 01:54:22 +0530

    Removed file from tracking

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-    await mongoose.connect(
-        "mongodb+srv://namastenodejs:WJmjh205ngCMUyar@namastenodejs.0otydp1.mongodb.net/DevTinder"
-    )
-};
-
-module.exports = connectDB;
